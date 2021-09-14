d3e689849b0412a765db258fb8b9538a3539e674
matthewsung <jooyongs1@gmail.com>
2021-09-14 12:54:26 +0900

    9.14

---
+++ third/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">ABOUT</router-link>
+    <router-view></router-view>
+  </div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ third/src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>ABOUT 페이지 입니다.</h2>
+    <h1>{{$store.state.count}}</h1>
+  </div>
+</template>
